5e8969ef8b52f539760775e01597795c8fb4b7b1
Eugenia Dzynia <eugenia.dzynia@edaider.com>
2023-03-08 19:13:55 +0100

    updated the name of hook

---
+++ src/shared/useCounter.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export const useCounter = (initialValue, delta, urlLength) => {
+  const [value, setValue] = useState(initialValue);
+
+  const INCREMENT = () => {
+    if (value < urlLength - 1) {
+      setValue((prev) => prev + delta);
+    }
+  };
+
+  //slide left, DECREMENT
+  const DECREMENT = () => {
+    if (value === 0) {
+      return;
+    } else {
+      setValue((prev) => prev - delta);
+    }
+  };
+
+  return {
+    value,
+    INCREMENT,
+    DECREMENT,
+    urlLength,
+  };
+};
